398b497b4ab470b8796cdd042887243b3307be61
Rafael Martinez Mena <rmartinez@cgclatam.com>
2018-09-24 13:03:05 -0600

    Cambios para contenedor principal-framelayout

--- app/src/main/java/usonsonate/com/tukybirth/Fragmento01.java
+++ app/src/main/java/usonsonate/com/tukybirth/Fragmento01.java
@@ -1,30 +1,25 @@
 package usonsonate.com.tukybirth;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.util.Pair;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class Fragmento01 extends Fragment {
     private CardView Procesar;
     private View view;
+    private Toolbar toolbar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_fragmento01, container, false);
-
         Procesar = view.findViewById(R.id.btnactivitycalendar);
         Procesar.setOnClickListener(new View.OnClickListener()
         {
