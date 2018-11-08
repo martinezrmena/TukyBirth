4fce0d254ad98b9c9a69a0d1f122037ad411a73b
Rafael Martinez Mena <rmartinez@cgclatam.com>
2018-11-08 14:32:06 -0600

    CRUD para manipular NOTAS

---
+++ app/src/main/java/usonsonate/com/tukybirth/Adaptadores/AdaptadorNotas.java
@@ -0,0 +1,45 @@
+package usonsonate.com.tukybirth.Adaptadores;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import usonsonate.com.tukybirth.R;
+import usonsonate.com.tukybirth.SQLite.Notas;
+
+public class AdaptadorNotas extends ArrayAdapter<Notas> {
+
+    public AdaptadorNotas(Context context, List<Notas> objects) {
+            super(context, 0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+            // Obteniendo el dato
+            Notas nota = getItem(position);
+            //TODO inicializando el layout correspondiente al item (Contacto)
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_nota, parent, false);
+            }
+
+            TextView lblId = (TextView) convertView.findViewById(R.id.lblId);
+            TextView lblNota = (TextView) convertView.findViewById(R.id.lblNota);
+            String p = String.valueOf(position+1);
+
+
+            // mostrar los datos
+
+            lblId.setText(p);
+            lblNota.setText(nota.getNota());
+
+            // Return la convertView ya con los datos
+            return convertView;
+        }
+}
---
+++ app/src/main/java/usonsonate/com/tukybirth/ListaNotasDia.java
@@ -0,0 +1,165 @@
+package usonsonate.com.tukybirth;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import usonsonate.com.tukybirth.Adaptadores.AdaptadorNotas;
+import usonsonate.com.tukybirth.SQLite.DB;
+import usonsonate.com.tukybirth.SQLite.Notas;
+
+
+public class ListaNotasDia extends AppCompatActivity {
+
+    private ArrayList<Notas> listaNotas, listaNotasNuevas;
+    private AdaptadorNotas adaptadorNotas;
+    private ListView lstNotas;
+    private String MES[] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+    private int mes;
+    private FloatingActionsMenu btnMenu;
+    private FloatingActionButton btnUpdateNote, btnDeleteNote;
+    private Boolean Modificar, Eliminar;
+    private String Message = "";
+    private DB db;
+    public static final int NOTA_MODIFICAR = 78;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas_dia);
+
+        lstNotas = findViewById(R.id.lstNotasDia);
+        btnMenu = findViewById(R.id.btnMainButton);
+        btnUpdateNote = findViewById(R.id.btnUpdateNote);
+        btnDeleteNote = findViewById(R.id.btnDeleteNote);
+
+        db = new DB(ListaNotasDia.this);
+
+        Modificar = false;
+        Eliminar = false;
+        Bundle contenedor = getIntent().getExtras();
+        listaNotas = (ArrayList<Notas>) contenedor.getSerializable("array");
+        listaNotasNuevas  = new ArrayList<>();
+
+
+        //creo el adaptador
+        adaptadorNotas = new AdaptadorNotas(ListaNotasDia.this, listaNotasNuevas );
+        //le establezco el adaptador al Listview
+        lstNotas.setAdapter(adaptadorNotas);
+
+        FillListEncuestado();
+
+        Calendar calendar = convertirACalendar(listaNotas.get(0).getFechanota());
+
+        setTitle(MES[calendar.get(Calendar.MONTH)]+ ", " + calendar.get(Calendar.DAY_OF_MONTH) + " - " + calendar.get(Calendar.YEAR ));
+
+        lstNotas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, final int position, long l) {
+                int id = 0;
+
+                if (Eliminar || Modificar){
+                    final Notas lastnota = listaNotas.get(position);
+
+                    if (Eliminar){
+                        Message = "¿Esta seguro de eliminar la nota?";
+                        id = R.drawable.ic_delete_note;
+                    }else if(Modificar){
+                        Message = "¿Esta seguro de modificar la nota?";
+                        id = R.drawable.update_note;
+                    }
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ListaNotasDia.this);
+                    builder.setIcon(id).
+                            setTitle("Atención").setMessage(Message).setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            if (Eliminar){
+                                db.borrarNota(String.valueOf(lastnota.getId()));
+                                Toast.makeText(ListaNotasDia.this, "Nota eliminada exitosamente.", Toast.LENGTH_SHORT ).show();
+                            }else if(Modificar){
+                                Intent intent = new Intent(ListaNotasDia.this, ModificarNota.class);
+                                intent.putExtra("NOTA", lastnota);
+                                startActivity(intent);
+                            }
+
+                            Intent resultIntent = new Intent();
+                            setResult(MainNotes.LIST_NOTES, resultIntent);
+                            finish();
+
+                        }
+                    }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Toast.makeText(ListaNotasDia.this, "Cancelaste la acción.", Toast.LENGTH_SHORT ).show();
+                            Modificar = false;
+                            Eliminar = false;
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+
+                }else{
+                    Toast.makeText(ListaNotasDia.this, "Primero debe seleccionar una acción a realizar.", Toast.LENGTH_SHORT ).show();
+                }
+
+
+            }
+        });
+    }
+
+    private void FillListEncuestado(){
+        for (final Notas c:listaNotas){
+            listaNotasNuevas.add(c);
+            adaptadorNotas.notifyDataSetChanged();
+        }
+    }
+
+    private Calendar convertirACalendar(String fecha){
+        String[] fechArray = fecha.split("-");
+
+
+        int anio = Integer.parseInt(fechArray[0]);
+        int mes =  Integer.parseInt(fechArray[1]) - 1;
+        int dia =  Integer.parseInt(fechArray[2]);
+
+        /*
+         *
+         * Al mes lo resto 1 (-1) ya que el formato de Calendar el mes empieza en 0
+         * Enero=0, Febrero=1, Marzo=2, ... , Diciembre=11
+         * De lo contrario Diciembre (12) no funcionaria
+         *
+         * */
+
+        Calendar c1 = new GregorianCalendar(anio, mes, dia);
+
+        return c1;
+    }
+
+    public void btnActualizarOnClick(View view){
+        Toast.makeText(this, "Pulsa una nota para actualizar.", Toast.LENGTH_SHORT ).show();
+        Modificar = true;
+        Eliminar = false;
+    }
+
+    public void btnEliminarOnClick(View view){
+        Toast.makeText(this, "Pulsa una nota para eliminarla.", Toast.LENGTH_SHORT ).show();
+        Eliminar = true;
+        Modificar = false;
+    }
+}
