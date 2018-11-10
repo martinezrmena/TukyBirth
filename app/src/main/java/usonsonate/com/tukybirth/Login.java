ca9200d680d0e42e5949b3f0be8dd90f820f0cc3
Rafael Martinez Mena <rmartinez@cgclatam.com>
2018-11-10 00:01:41 -0600

    Login primera parte

--- app/src/main/java/usonsonate/com/tukybirth/Login.java
+++ app/src/main/java/usonsonate/com/tukybirth/Login.java
@@ -1,13 +1,21 @@
 package usonsonate.com.tukybirth;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Login extends AppCompatActivity {
 
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        collapsingToolbarLayout = findViewById(R.id.collapsedToolBar);
+
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppBar);
     }
 }
